12c23d373e0283afb10bf89b5095e02d43e63c53
Shamil0Zamaliev <114811430+Shamil0Zamaliev@users.noreply.github.com>
2023-05-28 14:04:28 +0300

    Add files via upload

---
+++ 1coursec++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int** fromFile(size_t m, size_t n)
+{
+	ifstream in("data.txt");
+	in.seekg(3);
+
+	int** matrix = new int* [m];
+	for (size_t i = 0; i < m; i++)
+	{
+		matrix[i] = new int[n];
+		for (size_t j = 0; j < n; j++)
+			in >> matrix[i][j];
+	}
+
+	in.close();
+	return matrix;
+}
+
+void Calculate(int** matrix, int* vecS, size_t m, size_t n)
+{
+	for (size_t i = 0; i < m; ++i) {
+		vecS[i] = 0;
+		for (size_t j = 0; j < n; ++j) {
+			vecS[i] += matrix[i][j];
+		}
+	}
+}
+
+/*
+void Show(int** matrix, size_t m, size_t n)
+{
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+*/
+
+int main()
+{
+	ifstream file("data.txt");
+	size_t M;
+	size_t N;
+	file >> M;
+	file >> N;
+	int** Mat = fromFile(M, N);
+	int* Sum = new int[M];
+	Calculate(Mat, Sum, M, N);
+	for (size_t i = 0; i < N; i++)
+		cout << Sum[i] << " ";
+	cout << endl;
+
+
+
+
+
+
+
+
+}
